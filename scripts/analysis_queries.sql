45a69f73c03851aa90195bfac1a9ad544a87f962
Jiyaaarora <jiyaarora7675@gmail.com>
2026-03-19 14:55:02 +0530

    Create analysis_queries.sql

---
+++ scripts/analysis_queries.sql
@@ -0,0 +1,66 @@
+/*
+========================================
+Netflix Data Analysis using SQL
+========================================
+Author: Your Name
+Description:
+Basic Exploratory Data Analysis (EDA) on cleaned Netflix dataset
+========================================
+*/
+
+-- 1. Total Records
+SELECT
+    COUNT(*) AS total_records
+FROM vw_netflix_clean;
+
+
+-- 2. Movies vs TV Shows Count
+SELECT
+    type,
+    COUNT(*) AS total_count
+FROM vw_netflix_clean
+GROUP BY type;
+
+
+-- 3. Movies vs TV Shows Percentage
+SELECT
+    type,
+    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM vw_netflix_clean), 2) AS percentage
+FROM vw_netflix_clean
+GROUP BY type;
+
+
+-- 4. Top 5 Countries by Content
+SELECT TOP 5
+    country,
+    COUNT(*) AS total_content
+FROM vw_netflix_clean
+GROUP BY country
+ORDER BY total_content DESC;
+
+
+-- 5. Content Added Per Year (Recent First)
+SELECT
+    release_year,
+    COUNT(*) AS total_content
+FROM vw_netflix_clean
+GROUP BY release_year
+ORDER BY release_year DESC;
+
+
+-- 6. Most Common Ratings
+SELECT
+    rating,
+    COUNT(*) AS total_count
+FROM vw_netflix_clean
+GROUP BY rating
+ORDER BY total_count DESC;
+
+
+-- 7. Top 5 Genres
+SELECT TOP 5
+    listed_in AS genre,
+    COUNT(*) AS total_count
+FROM vw_netflix_clean
+GROUP BY listed_in
+ORDER BY total_count DESC;
